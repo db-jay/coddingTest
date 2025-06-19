8f8453400beb087a57008df86e764bd3c96ea5fa
Jay <devcinne@gmail.com>
2025-06-19 22:16:07 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -24,14 +24,29 @@
 # 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
 
 
+
 select 
     b.TITLE, 
     b.BOARD_ID, 
     r.REPLY_ID, 
     r.WRITER_ID, 
-    r.CONTENTS, 
+    r.CONTENTS,
     DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
 from USED_GOODS_BOARD b
-inner join USED_GOODS_REPLY r on b.board_id = r.board_id
-where YEAR(b.CREATED_DATE) = 2022 AND MONTH(b.CREATED_DATE) = 10
-order by r.CREATED_DATE asc, b.title asc;
+inner join USED_GOODS_REPLY r on b.BOARD_ID = r.BOARD_ID
+where YEAR(b.CREATED_DATE) = 2022 and MONTH(b.CREATED_DATE) = 10
+order by r.CREATED_DATE asc, b.TITLE asc
+
+
+
+# select 
+#     b.TITLE, 
+#     b.BOARD_ID, 
+#     r.REPLY_ID, 
+#     r.WRITER_ID, 
+#     r.CONTENTS, 
+#     DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+# from USED_GOODS_BOARD b
+# inner join USED_GOODS_REPLY r on b.board_id = r.board_id
+# where YEAR(b.CREATED_DATE) = 2022 AND MONTH(b.CREATED_DATE) = 10
+# order by r.CREATED_DATE asc, b.title asc;
